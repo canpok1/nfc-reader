fe510ec3cf33e9f68d8c6f925e8e6e6db79d6acd
canpok1 <6isstrong@gmail.com>
2020-06-21 03:12:16 +0900

    NFCタッチ時にファイルに情報を出力する機能を追加

--- nfc_reader.py
+++ nfc_reader.py
@@ -3,11 +3,13 @@ import nfc
 import os
 import RPi.GPIO as GPIO
 import time
+import datetime
 
 LED_RED_PIN = 11
 LED_GREEN_PIN = 12
 BUZZER_PIN = 32
 BUZZER_FREQ = 1200
+OUTPUT_DIR = '/tmp/nfc-reader'
  
 class MyCardReader(object):
     def __init__(self):
@@ -22,17 +24,8 @@ class MyCardReader(object):
         time.sleep(0.1)
         self.buzzer.stop()
  
-        #タグ情報を全て表示 
-        print(tag)
- 
-        #IDmのみ取得して表示 
-        self.idm = binascii.hexlify(tag._nfcid)
-        print("IDm : " + str(self.idm))
- 
-        #特定のIDmだった場合のアクション 
-        if self.idm == "00000000000000":
-            print("【 登録されたIDです 】")
- 
+        self.save_touched_log(tag)
+        
         return True
  
     def read_id(self):
@@ -41,6 +34,24 @@ class MyCardReader(object):
             clf.connect(rdwr={'on-connect': self.on_connect})
         finally:
             clf.close()
+
+    def save_touched_log(self, tag):
+        try:
+            #タグ情報を全て表示 
+            print(tag)
+ 
+            #IDmのみ取得して表示 
+            idm = binascii.hexlify(tag._nfcid).decode("UTF-8")
+            print("IDm : " + str(idm))
+ 
+            now = datetime.datetime.now()
+            log_path = OUTPUT_DIR + '/' + idm + '_' + now.strftime('%Y%m%d_%H%M%S')
+            print("log path : " + log_path)
+
+            with open(log_path, mode='w') as f:
+                f.write(str(tag))
+        except Exception as e:
+            print(e)
  
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BOARD)
@@ -49,6 +60,8 @@ if __name__ == '__main__':
     GPIO.setup(BUZZER_PIN, GPIO.OUT)
 
     try:
+        os.makedirs(OUTPUT_DIR, exist_ok=True)
+
         cr = MyCardReader()
         while True:
             #最初に表示 
