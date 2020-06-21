097c9b99162068299256309bd6e7a66a1b3cf5fc
canpok1 <6isstrong@gmail.com>
2020-06-21 11:42:39 +0900

    NFC情報の送信プログラムのsystemd登録用ファイルを追加

--- make_systemd_file.sh
+++ make_systemd_file.sh
@@ -10,8 +10,35 @@ Description = nfc reader
 ExecStart = ${dir}/start_nfc_reader.sh
 Restart = always
 Type = simple
+StandardOutput = append:/var/log/nfc-reader.log
+StandardError = append:/var/log/nfc-reader.log
 
 [Install]
 WantedBy = multi-user.target
+EOS
+
+cat << EOS > /etc/systemd/system/nfc-reader-sender.service
+[Unit]
+Description = nfc reader sender
+
+[Service]
+ExecStart = ${dir}/start_nfc_reader_sender.sh
+Type = oneshot
+User = pi
+Group = pi
+StandardOutput = append:/var/log/nfc-reader-sender.log
+StandardError = append:/var/log/nfc-reader-sender.log
+EOS
+
+cat << EOS > /etc/systemd/system/nfc-reader-sender.timer
+[Unit]
+Description = schedule nfc reader sender
+
+[Timer]
+Unit=nfc-reader-sender.service
+OnCalendar=*:0/3
+
+[Install]
+WantedBy=timers.target
 EOS
 
---
+++ start_nfc_reader_sender.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd $(dirname $0)
+python3 $(pwd)/nfc_reader_sender.py
