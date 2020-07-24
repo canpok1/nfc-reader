141859c735c8de9daa0b83a6b8d4636ff01904f4
canpok1 <6isstrong@gmail.com>
2020-07-25 02:58:03 +0900

    systemdでサービスアカウントのcredential読めるよう修正

--- make_systemd_file.sh
+++ make_systemd_file.sh
@@ -2,16 +2,24 @@
 cd $(dirname $0)
 dir=$(pwd)
 
+mkdir -p /etc/sysconfig
+cat << EOS > /etc/sysconfig/nfc-reader-env
+GOOGLE_APPLICATION_CREDENTIALS=${dir}/credentials/gcp-service-account-cred.json
+EOS
+
 cat << EOS > /etc/systemd/system/nfc-reader.service
 [Unit]
 Description = nfc reader
 
 [Service]
+EnvironmentFile = /etc/sysconfig/nfc-reader-env
 ExecStart = ${dir}/start_nfc_reader.sh
 Restart = always
 Type = simple
 StandardOutput = append:/var/log/nfc-reader.log
 StandardError = append:/var/log/nfc-reader.log
+User = pi
+Group = pi
 
 [Install]
 WantedBy = multi-user.target
@@ -24,10 +32,10 @@ Description = nfc reader sender
 [Service]
 ExecStart = ${dir}/start_nfc_reader_sender.sh
 Type = oneshot
-User = pi
-Group = pi
 StandardOutput = append:/var/log/nfc-reader-sender.log
 StandardError = append:/var/log/nfc-reader-sender.log
+User = pi
+Group = pi
 EOS
 
 cat << EOS > /etc/systemd/system/nfc-reader-sender.timer
